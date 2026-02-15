efdc60653802e43dcd9cdde3aea4a38a8536f65d
Serge <105081370+Ceziy01@users.noreply.github.com>
2026-02-16 00:21:15 +0300

    -

--- app/models.py
+++ app/models.py
@@ -8,3 +8,12 @@ class Users(Base):
     username = Column(String, unique=True, nullable=False)
     hashed_password = Column(String, nullable=False)
     is_admin = Column(Boolean, default=False)
+
+class Items(Base):
+    __tablename__ = "items"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, nullable=False)
+    count = Column(Integer, nullable=False)
+    price = Column(Integer, nullable=False)
+    on_sale = Column(Boolean, default=True)
